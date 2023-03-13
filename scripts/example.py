8befea7dffdb32cf733b1eda03f5fb350e6ec9af
andzh1 <andrey.zhizhelev@yandex.ru>
2023-03-13 17:08:33 +0300

    Update

--- scripts/example.py
+++ scripts/example.py
@@ -3,7 +3,6 @@ from create_metadata import create_metadata_for_vacancy
 company_name = 'Yandex'
 vacancy_name = 'SWE intern'
 ya_logo = 'https://raw.githubusercontent.com/TheBestID/ton-contracts/main/images/data/Yandex_logo.jpeg'
-
 wallet = "kQBf4BVi8IQ5iILFqsr0WHnyg1VRgZRBbjBv_ji5jc6Urpqq"
 applicant_wallet = "kQBf4BVi8IQ5iILFqsr0WHnyg1VRgZRBbjBv_ji5jc6Urpqq"
 referral_wallet = "kQBf4BVi8IQ5iILFqsr0WHnyg1VRgZRBbjBv_ji5jc6Urpqq"
